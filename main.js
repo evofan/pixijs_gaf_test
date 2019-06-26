a1be5076d1bfb9fefb9baecc7d0a002cf3ff4432
evofan <evofan@outlook.jp>
2019-06-26 14:27:19 +0900

    add get callbackEvent sample

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ let app = new PIXI.Application({
   forceCanvas: false,
   transparent: true
 });
-document.body.appendChild(app.view);
-app.renderer.backgroundColor = "#3366cc";
+let canvas1 = document.getElementById("canvas1");
+canvas1.appendChild(app.view);
 app.stage.interactive = true;
 
-let x_pos = [0, 0, 200, 400];
-let y_pos = [0, -100, 100, 100];
+let app2 = new PIXI.Application({
+  width: 240,
+  height: 240,
+  transparent: false
+});
+let canvas2 = document.getElementById("canvas2");
+app2.renderer.backgroundColor = "#ffffff";
+app2.stage.interactive = true;
+canvas2.appendChild(app2.view);
+
+let x_pos = [0, 0, 200, 400, 0];
+let y_pos = [0, -100, 100, 100, 0];
 let mc_max = 3;
 
 // load gaf
@@ -33,6 +43,12 @@ let converter3 = new GAF.ZipToGAFAssetConverter();
 converter3.once(GAF.GAFEvent.COMPLETE, onConverted(3));
 converter3.convert(FILE_NAME3 + "/" + FILE_NAME3 + ".gaf");
 
+// 4
+let FILE_NAME4 = "omikuji_03";
+let converter4 = new GAF.ZipToGAFAssetConverter();
+converter4.once(GAF.GAFEvent.COMPLETE, onConverted(4));
+converter4.convert(FILE_NAME4 + "/" + FILE_NAME4 + ".gaf");
+
 /**
  * set gaf animation
  * @param { number } [id=0] generate a name for movie clip
@@ -48,8 +64,15 @@ function onConverted(id = 0) {
     gafMovieClip.name = `mc_${id}`;
     gafMovieClip.x = x_pos[id];
     gafMovieClip.y = y_pos[id];
-    gafMovieClip.play();
-    app.stage.addChild(gafMovieClip);
+
+    if (id === 4) {
+      gafMovieClip.stop();
+      app2.stage.addChild(gafMovieClip);
+      gafMovieClip.on("getResult", getCallbackEvent);
+    } else {
+      gafMovieClip.play();
+      app.stage.addChild(gafMovieClip);
+    }
   };
 }
 
@@ -67,4 +90,22 @@ function changeAnimation(anim_name, mc_name) {
   if (mc[0]) {
     mc[0].gotoAndPlay(anim_name);
   }
+}
+
+/**
+ * start animation to get callback
+ */
+function startAnimation() {
+  let mc = app2.stage.children[0];
+  console.log(mc);
+  mc.gotoAndPlay("start_anime");
+}
+
+/**
+ * get callback event from .gaf timeline
+ * @param { object } pEvent .gaf event
+ */
+function getCallbackEvent(pEvent) {
+  console.log("getCallbackEvent()", pEvent, pEvent.data);
+  alert("get callback event from .gaf timeline");
 }
